0bbceba1050dc68bcdd91f7ae00dd51e160c58d9
Iram Liu <il233@cornell.edu>
2023-03-21 00:21:35 -0400

    Implemented id assignment to players to help with turn transitions

--- src/state.ml
+++ src/state.ml
@@ -22,3 +22,16 @@ let init_state cards = { deck = cards; players = []; current_player = 0 }
 
 let add_player game_state player =
   { game_state with players = player :: game_state.players }
+
+let rec assign_id_rec num p_list : player list =
+  match p_list with
+  | [] -> p_list
+  | h :: t -> assign_id_rec (num + 1) ({ h with id = num } :: t)
+
+let assign_id game_state num =
+  { game_state with players = assign_id_rec num game_state.players }
+
+(*Need to change the mod 4 to the number of players but we default to 4 players
+  for now*)
+let next_turn game_state =
+  { game_state with current_player = (game_state.current_player + 1) mod 4 }
