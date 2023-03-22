89fdf5e675ddabbbc5c5efdb4eab697a33e74603
Iram Liu <il233@cornell.edu>
2023-03-22 01:29:49 -0400

    Finished functionalities for MS2 Handled more user inputs to terminal, now shows hands of users.

--- src/initialize.mli
+++
@@ -1,4 +0,0 @@
-type player
-
-val fake_list : int list
-val initialize_players_hands : player list -> int list -> player list
--- src/state.mli
+++ src/state.mli
@@ -4,3 +4,9 @@ type state
 val init_player : string -> player
 val add_player : state -> player -> state
 val init_state : state
+val getPlayerList : state -> player list
+val fake_list : int list
+val initialize_players_hands : player list -> int list -> player list
+val updatePlayers : state -> player list -> state
+val getPlayerHand : player -> int list
+val getPlayerName : player -> string
