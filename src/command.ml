8d13fe14a40a4c1d1b62cf00f5588cb041f219d3
Joanna Lin <jl2748@cornell.edu>
2023-04-24 00:44:03 -0400

    Edited command.mli and command.ml files: - added types and parse function

--- src/command.ml
+++ src/command.ml
@@ -1,2 +1,16 @@
+type object_tuple = string * int
+type command = Request of object_tuple
+
 exception Unrecognized
 exception Empty
+
+let rec parse_helper str =
+  let split = String.split_on_char ' ' str in
+  let filtered = List.filter (fun item -> item <> "") split in
+  match filtered with
+  | [] -> raise Empty
+  | [ h; t1; t2 ] when h = "request" && List.length filtered > 2 ->
+      Request (t1, int_of_string t2)
+  | _ -> raise Unrecognized
+
+let parse str = parse_helper str
