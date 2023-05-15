7f33181da12d224f30e66cdf06b13394ad557050
Iram Liu <il233@cornell.edu>
2023-05-15 00:58:41 -0400

    Fixed bugs in check_winner

--- bin/main.ml
+++ bin/main.ml
@@ -132,7 +132,6 @@ let rec shift_ready state num input =
   match input with
   | i when String.lowercase_ascii i = "ready" ->
       let _ = print_log (List.rev (State.get_log state)) 10 in
-      let _ = print_deck (State.get_deck state) in
       state
   | i when String.lowercase_ascii i = "quit" ->
       print_endline "Farewell Go Fish-ers ";
@@ -247,7 +246,8 @@ let rec game_cycle (state : State.state) num =
     with Command.Unrecognized -> game_cycle state num
   else (
     print_endline "No more cards left in the deck. The game is over";
-    print_endline (State.check_winner state |> print_winner);
+    print_string "The winner is: ";
+    print_endline (State.check_winner state |> String.concat " ");
     exit 0)
 
 let rec move_next state num input =
